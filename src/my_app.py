c27eb9284f09e4136b62ee903a897ea7599628e0
Alvaro Echague <alvaro@primer.io>
2022-04-28 12:29:16 +0100

    Adding Duplicated code

--- src/my_app.py
+++ src/my_app.py
@@ -7,6 +7,12 @@ def my_app(num):
     else:
         return False
 
+def duplicate_my_app(num):
+    if num > 0:
+        return True
+    else:
+        return False
+
 def poor_code():
     # foo is not defined
     foo()
