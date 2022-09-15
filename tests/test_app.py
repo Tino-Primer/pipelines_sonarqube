c6ce702e3233b61262333a93e2e539cefdc831a2
Tino-Primer <alvaro@primer.io>
2022-09-15 12:47:30 +0200

    adding tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from src.my_app import my_app
+from src.my_app import my_app, poor_code, bad_bad_code
 
 
 def test_num_more_than_zero():
@@ -12,3 +12,9 @@ def test_num_is_zero():
 
 def test_num_is_float():
     assert my_app(0.5) == True
+
+def test_poor_code():
+    assert poor_code() == "poor code"
+
+def test_bad_bad_code():
+    assert bad_bad_code() == "bad bad code"
