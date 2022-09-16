f898a71cecc6719b6e4956af3575c4489dd83a53
Tino-Primer <alvaro@primer.io>
2022-09-16 10:33:50 +0200

    anotehr update

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 from src.my_app import my_app, poor_code, bad_bad_code
+from src.new_file import *
 
 
 def test_num_more_than_zero():
@@ -18,3 +19,6 @@ def test_poor_code():
 
 def test_bad_bad_code():
     assert bad_bad_code() == "bad bad code"
+
+def test_new_func():
+    assert tino_new_func() == "hello"
